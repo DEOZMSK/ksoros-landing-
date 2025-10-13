70c12a42c4fec9abf07016d8e560f82c203b023d
DEOZMSK <jhfc47b67x3782@gmail.com>
2025-10-13 19:58:31 +0300

    Удалил скрипты аналитики и связанные переменные

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import { Onest, Unbounded } from "next/font/google";
 import "./globals.css";
 
@@ -17,8 +16,6 @@ const unbounded = Unbounded({
 });
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://example.com"; // TODO: заменить на боевой домен
-const yandexMetrikaId = process.env.NEXT_PUBLIC_YANDEX_METRIKA_ID;
-const gaMeasurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
@@ -53,43 +50,6 @@ export default function RootLayout({
     <html lang="ru" className={`${onest.variable} ${unbounded.variable}`}>
       <body className="bg-background">
         {children}
-        {yandexMetrikaId && (
-          <Script id="yandex-metrika" strategy="afterInteractive">
-            {`
-              (function(m,e,t,r,i,k,a){
-                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0];
-                k.async=1;k.src=r;a.parentNode.insertBefore(k,a);
-              })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-              ym(${yandexMetrikaId}, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                ecommerce:"dataLayer"
-              });
-            `}
-          </Script>
-        )}
-        {gaMeasurementId && (
-          <>
-            <Script
-              id="google-analytics"
-              strategy="afterInteractive"
-              src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
-            />
-            <Script id="google-analytics-inline" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${gaMeasurementId}', {
-                  send_page_view: true
-                });
-              `}
-            </Script>
-          </>
-        )}
       </body>
     </html>
   );
